786094fb6917f90dec1012e2a8caf519ab6e5040
Marc Altmann <marcaltmann@posteo.de>
2017-11-09 00:27:01 +0100

    Display first hanzi

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
+import hanziList from '../data/hanzi-list';
 
 const App = () => (
-  <div>
-    Hello
-  </div>
+  <ul>
+    {
+      hanziList.map((hanzi, index) => (
+        <li key={index}>
+          {hanzi.t}
+        </li>
+      ))
+    }
+  </ul>
 );
 
 export default App;
--- src/reducers/position.test.js
+++ src/reducers/position.test.js
@@ -1,22 +1,12 @@
-const should = require('should');
+import 'should';
+import reducer, { getPosition } from './position';
 
-describe('Array', () => {
-  describe('#indexOf()', () => {
-    it('should return -1 when the value is not present', () => {
-      [1, 2, 3].indexOf(4).should.be.exactly(-1);
-    });
-  });
-
-  describe('#splice()', () => {
-    it('should alter original array', () => {
-      const original = [1, 2, 3, 4, 5];
-      original.splice(2, 2);
-      original.should.eql([1, 2, 5]);
-    });
-
-    it('should return spliced portion', () => {
-      const original = [1, 2, 3, 4, 5];
-      original.splice(2, 2).should.eql([3, 4]);
+describe('position reducer', function() {
+  describe('getPosition()', function() {
+    it('should return the current position', function() {
+      const state = 0;
+      const result = getPosition(state);
+      result.should.be.exactly(0);
     });
   });
 });
