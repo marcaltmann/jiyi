be6eb5947e1e4dd7662b0853cc38cf1ef2ceb18f
Marc Altmann <marcaltmann@posteo.de>
2017-11-08 23:39:28 +0100

    Install eslint

---
+++ .eslintrc.js
@@ -0,0 +1,60 @@
+const config = {
+  globals: {
+    '__VERSION__': false,
+    'process': false,
+    'module': false,
+    '__dirname': true,
+  },
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+  ],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      experimentalObjectRestSpread: true,
+    },
+    ecmaVersion: 2017,
+  },
+  rules: {
+    indent: [
+      'error',
+      2,
+    ],
+    'linebreak-style': [
+      'error',
+      'unix',
+    ],
+    quotes: [
+      'error',
+      'single',
+    ],
+    semi: [
+      'error',
+      'always',
+    ],
+    'no-unused-vars': [
+      'warn',
+      'all',
+    ],
+    'no-console': [
+      'error',
+      {
+        allow: [
+          'warn',
+          'error',
+        ],
+      }
+    ],
+  },
+  plugins: [
+    'react',
+  ],
+};
+
+module.exports = config;
